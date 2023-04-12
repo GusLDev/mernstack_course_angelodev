1c397610f5d9f35d4b1f56c16b75657bbc46684d
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-11 23:35:44 -0700

    Database connection by mongo Atlas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,16 +1,7 @@
-const mongoose = require('mongoose')
-
 //CONECTION -->
-const URI = process.env.MONGODB_URI 
-? process.env.MONGODB_URI 
-: 'mongodb://localhost/dbtest'
+const mongoose = require("mongoose");
 
-//If database doesnt exist it will create that db in enviroment file
-
-mongoose.connect(URI)
-const connection = mongoose.connection;
-console.log('Llega A la moongose conection')
-console.log(URI)
-connection.once('open',()=>{
-    console.log('La base de datos ha sido conectada: ',URI);
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((res) => console.log("connected"))
+  .catch((e) => console.log("error: " + e));
