0d395fde4d54893bcb0937d10741dfad138a71c9
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-12 22:42:27 -0700

    Middlewares Done

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,4 +6,12 @@ const app = express(); //Stores What express Has
 //Configuration
 app.set('port',process.env.PORT || 4000) //THIS IS FOR SAVE A VARIABLE CALLED PORT
 
+
+//Middlewares
+app.use(cors()) //This for get request for other servers that are not our backend server
+app.use(express.json()) //This is for validate json's send and get
+//Rutas
+app.get('/',(req,res)=>{
+    res.send('Bienvenido ami api rest full');
+})
 module.exports = app;
