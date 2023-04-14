b9bece341a9f59427e7c3e8187ac2ca43955531b
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-14 16:21:19 -0700

    Request put and delete completed

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -2,7 +2,7 @@ const userCtrl = {}
 //WE ARE GETTING OUR USER MODEL
 const User = require('../models/User')
 
-userCtrl.getUser = async(req,res) =>{
+userCtrl.getUsers = async(req,res) =>{
     const users = await User.find() //WE ARE LOOKING FOR ALL USERS
     res.json(users) //THIS IS THE ANSWER
 }
@@ -17,19 +17,28 @@ userCtrl.createUser = async(req,res) =>{
         email: email
     })
     await newUser.save(); //WE SAVE A NEW DOCUMENT WITH THE NEW INFORMATION FOR A USER HERE
-    res.json({message: "El usuario ha sido creado"})
+    res.json({message: "The user has been created"})
 }
 
-userCtrl.getUsu = async(req,res) =>{
-    
+userCtrl.getUser = async(req,res) =>{
+    const user = await User.findById(req.params.id) //WE ONLY GET THE ID FROM req
+    res.json(user) //WE HAVE AN ANSWER AND IT'S THE USER
 }
 
-userCtrl.getUsu = async(req,res) =>{
-    
+userCtrl.deleteUser = async(req,res) =>{
+    await User.findByIdAndDelete(req.params.id) //WE FIND THE USER AND THEN WE ELIMINATED THAT USER
+    res.json({message: 'The user has been deleted'})
 }
 
-userCtrl.getUsu = async(req,res) =>{
-    
+userCtrl.updateUser = async(req,res) =>{
+    const {name,lastname,age,phone,email} = req.body; //THIS IS PROBABLY THE NEW INFORMATION FROM THE CLIENT
+    await User.findByIdAndUpdate(req.params.id,{
+        name,
+        lastname,
+        age,
+        phone,
+        email
+})
+    res.json({message: 'The user has been updated'})
 }
-
-
+module.exports = userCtrl;
