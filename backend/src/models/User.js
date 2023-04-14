64dd05709f7ce0330779ec2c39cf88d0880dcde1
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-14 15:54:02 -0700

    Schema changes

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,10 @@
 const {Schema,model} = require('mongoose') //We are going to use two functionalities from moongose
 const userSchema = new Schema({
-    nombre: String,
-    apellido: String,
-    edad: Number,
-    telefono: Number,
-    correo: String
+    name: String,
+    lastname: String,
+    age: Number,
+    phone: Number,
+    email: String
 },
 {
     timestamps: true
