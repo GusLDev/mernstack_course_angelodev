653b6c92e2ddb86647a139a0466bcd78e831f440
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-18 16:53:30 -0700

    Finished create user functionality

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,40 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const CreateUser = () => {
+  const initialValue = {
+    name: '',
+    lastname: '',
+    age: 18,
+    phone: 0,
+    email: ''
+  }
+  const[user,setUser] = useState(initialValue);
+
+  const captureData = (e) =>{
+    const{name,value} = e.target
+    setUser({...user,[name]: value})
+  }
+
+  const saveData = async(e) =>{
+    e.preventDefault();
+    //REQUEST TO POST
+    const newUser = {
+      name: user.name,
+      lastname: user.lastname,
+      age: user.age,
+      phone: user.phone,
+      email: user.email
+    }
+
+    await axios.post('http://localhost:4000/api/users',newUser)
+
+    setUser({...initialValue}) //RESET VALUES
+  }
+
   return (
     <div className='col-md-6 offset-md-3'>
       <div className='card card-body'>
-        <form>
+        <form onSubmit={saveData}>
           <h2 className='text-center mb-3'>Crear Usuario</h2>
           <div className='mb-3'>
             <label>
-              Nombre:
+              Name:
             </label>
-            <input type='text' className='form-control' placeholder='Introduce username' required />
+            <input type='text' className='form-control' 
+            placeholder='Introduce name' required
+            name='name' value={user.name}
+            onChange={captureData} />
           </div>
           <div className='mb-3'>
             <label>
-              Apellido:
+              Lastname:
             </label>
-            <input type='text' className='form-control' placeholder='Introduce user lastname' required />
+            <input type='text' className='form-control' 
+            placeholder='Introduce user lastname' required
+            name='lastname' value={user.lastname}
+            onChange={captureData}  />
           </div>
           <div className='mb-3'>
             <label>
               Age:
             </label>
-            <input type='number' className='form-control' placeholder='Introduce user age' required />
+            <input type='number' className='form-control' 
+            placeholder='Introduce user age' required 
+            name='age' value={user.age}
+            onChange={captureData} />
           </div>
           <div className='mb-3'>
             <label>
               Phone:
             </label>
-            <input type='number' className='form-control' placeholder='Introduce user phone number' required />
+            <input type='number' className='form-control' 
+            placeholder='Introduce user phone number' required
+            name='phone' value={user.phone} 
+            onChange={captureData} />
           </div>
           <div className='mb-3'>
             <label>
               Email:
             </label>
-            <input type='text' className='form-control' placeholder='Introduce user email' required />
+            <input type='text' className='form-control' 
+            placeholder='Introduce user email' required 
+            name='email' value={user.email}
+            onChange={captureData} />
           </div>
           <button className='btn btn-primary form-control'>Guardar Usuario</button>
         </form>
